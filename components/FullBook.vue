45979295d655c86953671cbab6ef809795d0bd07
awgraves <andrewgraves92@gmail.com>
2020-10-24 12:17:29 -0400

    initial page flipping working!

--- components/FullBook.vue
+++ components/FullBook.vue
@@ -1,26 +1,48 @@
 <template>
     <div class="wrapper">
         <div class="fullbook">
-            yo and some othessss
+            <Page :pageIndex="0"/>
+            <Page :pageIndex="1"/>
         </div>
     </div>
 </template>
 
 <script>
+import Page from './Page.vue';
+
 export default {
-    name: 'FullBook'
+    name: 'FullBook',
+    components: {'Page': Page},
 }
 </script>
 
 <style lang="scss">
+:root {
+    --book-height: 600px;
+    --book-width: 900px;
+}
+
 .wrapper {
     background-color: yellow;
     position: relative;
-    height: 80%;
-    width: 80%;
-    margin: auto;
+    box-shadow: 0 0 100px rgba(0, 0, 0, .3);
+    width: var(--book-width);
+    height: var(--book-height);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    // width: min(60%, 300);
+    // padding-top: 56.25%; /* 16:9 Aspect Ratio */
 }
 .fullbook {
-    background-color: white;
+    background-color: blue;  // color of the cover
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    -webkit-perspective: 1200px;
+    perspective: 1200px;
 }
+
 </style>
---
+++ components/Page.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="page" :class="{'page--flipped': isFlipped}" :style="{'z-index': pageZIndex}">
+        <div class="page__front" @click="flipPage">
+            <div>
+                front here
+            </div>
+        </div>
+        <div class="page__back" @click="flipPage">
+            <div>
+                back here
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Page',
+    props: ['pageIndex'],
+    created(){
+        // when page first created, calc the beginning Z index
+        this.pageZIndex = 999 - this.pageIndex;
+    },
+    data(){
+        return {
+            isFlipped: false,
+            pageZIndex: null,  // determines stacking order, calc on init
+        }
+    },
+    methods: {
+        flipPage(){
+            this.isFlipped = !this.isFlipped;
+            if(this.isFlipped){
+                setTimeout(()=>{this.pageZIndex = this.pageIndex;}, 1000);
+            }else{
+                this.pageZIndex = 999 - this.pageIndex;
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+$flipSpeed: 1s;
+
+.page {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    height: 100%;
+    width: 50%;
+    transition: transform $flipSpeed;
+    transform-style: preserve-3d;
+    transform-origin: left;
+    cursor: pointer;
+
+    &__front, &__back {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        -webkit-backface-visibility: hidden; /* Safari */
+        backface-visibility: hidden;
+        background-color: white;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+
+        div {
+            padding: 2ch;
+        }
+    }
+
+    &__front {
+        background-image: linear-gradient(90deg, rgba(227,227,227,1) 0%, rgba(247,247,247,0) 10%);
+    }
+
+    &__back {
+        transform: rotateY(180deg);
+        background-image: linear-gradient(-90deg, rgba(227,227,227,1) 0%, rgba(247,247,247,0) 10%);
+    }
+}
+.page--flipped {
+    transform-origin: left;
+    transform: rotate3D(0, 1, 0, -180deg);
+    transition: transform $flipSpeed;
+}
+</style>
